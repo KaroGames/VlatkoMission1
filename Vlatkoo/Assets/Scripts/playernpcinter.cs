90708d81ccac74d6593f1aa67a61d81be91633d5
bariseken <55782657+bariseken@users.noreply.github.com>
2019-12-30 22:21:48 +0300

    sasdad

---
+++ Vlatkoo/Assets/Scripts/playernpcinter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class playernpcinter : MonoBehaviour
+{
+
+    public GameObject interactNPCTEXT;
+    public GameObject DialogueTEXT;
+
+    private GameObject interactingNPC;
+    private bool eyebastim = false;
+    private bool kesisiyolar = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactNPCTEXT.SetActive(false);
+        DialogueTEXT.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kesisiyolar)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DialogueTEXT.SetActive(true);
+                interactNPCTEXT.SetActive(false);
+                eyebastim = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "NPC")
+        {
+            kesisiyolar = true;
+            interactNPCTEXT.SetActive(true);
+            interactingNPC = other.gameObject;
+            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                
+                Debug.Log("diasdasdasd");
+            }
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        kesisiyolar = false;
+
+        interactNPCTEXT.SetActive(false);
+        DialogueTEXT.SetActive(false);
+
+
+        
+    }
+}
---
+++ Vlatkoo/Assets/durdur.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;  
+
+public class durdur : MonoBehaviour
+{
+
+    private bool kesisme;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kesisme)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponentInParent<NavMeshAgent>().isStopped = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            kesisme = true;
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("durdu");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        kesisme = false;
+        GetComponentInParent<NavMeshAgent>().isStopped = false;
+    }
+}
